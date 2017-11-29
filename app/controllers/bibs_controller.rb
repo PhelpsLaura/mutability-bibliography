a524bb17bdb4a7f444eba751b16967b509a28a3f
Laura Phelps <laphelps64@gmail.com>
2017-11-29 15:16:10 -0500

    new and show temp displays

--- app/controllers/bibs_controller.rb
+++ app/controllers/bibs_controller.rb
@@ -1,2 +1,47 @@
 class BibsController < ApplicationController
+  def index
+    @bibs = Bibs.all
+  end
+
+  def show
+    @bib = Bibs.find(params[:id])
+  end
+
+  def new
+    @bib = Bib.new
+  end
+
+  def create
+    @bib = Bib.new(bib_params)
+    if @bib.save
+      redirect_to bib_path(@bib)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @bib = Bib.find(params[:id])
+  end
+
+  def update
+    @bib = Bib.find(params[:id])
+    if @bib.update(bib_params)
+      redirect_to bib_path(@bib)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @bib = Bib.find(params[:id])
+    @bib.destroy
+
+    redirect_to bibs_path
+  end
+
+  private
+    def bib_params
+      params.require(:bib).permit(:options)
+    end
 end
